518eb1a5e06d61f5b9913fecabca2f1f3051dd9d
CMT <cmt@CMTs-MacBook-Air.local>
2019-09-30 18:02:44 +0100

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,4 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen", "queen", "queen", "queen"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else alert("Sorry, try again");
+}
