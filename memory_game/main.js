d4f6fea24e826c79c2980fe2a6aa13613f5fd7e4
CMT <cmt@CMTs-MacBook-Air.local>
2019-09-30 20:38:22 +0100

    game logic added

--- memory_game/main.js
+++ memory_game/main.js
@@ -30,18 +30,3 @@ const flipCard = cardId => {
 };
 
 flipCard(1);
-
-// let cardOne = cards[0];
-// let cardTwo = cards[3];<p></p>
-
-// cardsInPlay.push(cardOne);
-// cardsInPlay.push(cardTwo);
-
-// console.log("User flipped " + cardOne);
-// console.log("User flipped " + cardTwo);
-
-// if (cardsInPlay.length === 2) {
-//   if (cardsInPlay[0] === cardsInPlay[1]) {
-//     alert("You found a match!");
-//   } else alert("Sorry, try again");
-// }
